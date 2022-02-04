4df8c67b9ebe94e3ac1fc012b6751edfb50f2271
sangkun1232 <99011213+sangkun1232@users.noreply.github.com>
2022-02-04 17:24:37 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+$('.wrap').fullpage({
+    navigation:true,
+    anchors:['1page','2page', '3page', '4page','5page']
+    
+});
+
+$('.img1').colorbox({
+    rel : 'img1'
+
+});
+$('.youtube').colorbox({
+      iframe:true, innerWidth:1200 , innerHeight:720 ,rel:'img1' 
+});
